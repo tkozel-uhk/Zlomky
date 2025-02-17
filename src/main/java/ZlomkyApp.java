8c5dd223718569af4b28c81ef821ffce4f93b8dd
tkozel <tom@kozlovi.net>
2025-02-17 11:26:37 +0100

    Arithmetic operations on Zlomek

--- src/main/java/ZlomkyApp.java
+++ src/main/java/ZlomkyApp.java
@@ -7,7 +7,14 @@ public class ZlomkyApp {
 
         //vytvoreni instance(=objektu) tridy ZLomek
         Zlomek a = new Zlomek(3, 4);
-
         System.out.printf("Zlomek a = %s\n", a);
+
+        Zlomek b = new Zlomek(2, 5);
+
+        Zlomek c = a.krat(b);
+        System.out.printf("a * b = %s\n", c.zkratit());
+        System.out.printf("a / b = %s\n", a.deleno(b).zkratit());
+        System.out.printf("a + b = %s\n", a.plus(b));
+        System.out.printf("a - b = %s\n", a.minus(b));
     }
 }
--- src/main/java/cz/uhk/zlomky/model/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/model/Zlomek.java
@@ -1,8 +1,8 @@
 package cz.uhk.zlomky.model;
 
 public class Zlomek {
-    private int citatel;
-    private int jmenovatel;
+    private final int citatel;
+    private final int jmenovatel;
 
     public Zlomek(int citatel, int jmenovatel) {
         this.citatel = citatel;
@@ -15,6 +15,43 @@ public class Zlomek {
 
     public int getJmenovatel() {
         return jmenovatel;
+    }
+
+    public Zlomek krat(Zlomek druhy) {
+        return new Zlomek(citatel * druhy.citatel, jmenovatel * druhy.jmenovatel);
+    }
+    public Zlomek deleno(Zlomek druhy) {
+        return new Zlomek(citatel * druhy.jmenovatel, jmenovatel * druhy.citatel);
+    }
+    public Zlomek plus(Zlomek druhy) {
+        int cit = citatel * druhy.jmenovatel + druhy.citatel*jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+    public Zlomek minus(Zlomek druhy) {
+        int cit = citatel * druhy.jmenovatel - druhy.citatel*jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek zkratit() {
+        int a = Math.abs(citatel);
+        int b = Math.abs(jmenovatel);
+        if (a < b) {
+            int pom = a;
+            a = b;
+            b = pom;
+        }
+
+        int zb;
+        do {
+            zb = a % b;
+            a = b;
+            b = zb;
+        } while (zb != 0);
+        return new Zlomek(citatel / a, jmenovatel / a);
     }
 
     @Override
