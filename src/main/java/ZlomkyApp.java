94da003697d129b7acf18b2ada686d3e72ba554b
tkozel <tom@kozlovi.net>
2025-02-24 10:50:29 +0100

    Doplnena suma pole zlomku, priprava JUnit/Test

--- src/main/java/ZlomkyApp.java
+++ src/main/java/ZlomkyApp.java
@@ -16,5 +16,24 @@ public class ZlomkyApp {
         System.out.printf("a / b = %s\n", a.deleno(b).zkratit());
         System.out.printf("a + b = %s\n", a.plus(b));
         System.out.printf("a - b = %s\n", a.minus(b));
+
+        Zlomek[] poleZlomku = new Zlomek[4];
+        poleZlomku[0] = a;
+        poleZlomku[1] = b;
+        poleZlomku[2] = c;
+        poleZlomku[3] = a.deleno(b);
+
+        Zlomek all = sumaZlomku(poleZlomku);
+        System.out.printf("Soucet je %s\n", all);
+    }
+
+    public static Zlomek sumaZlomku(Zlomek[] pole) {
+        Zlomek soucet = new Zlomek(0, 1);
+
+        for (Zlomek z : pole) {
+            soucet = soucet.plus(z);  //soucet += z
+        }
+
+        return soucet.zkratit();
     }
 }
---
+++ src/test/java/cz/uhk/zlomky/model/ZlomekTest.java
@@ -0,0 +1,41 @@
+package cz.uhk.zlomky.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ZlomekTest {
+
+    @Test
+    void krat() {
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(4, 5);
+        Zlomek c = a.krat(b);
+        assertEquals(12, c.getCitatel(), "Chybny citatel");
+        assertEquals(20, c.getJmenovatel(), "Chybny jmenovatel");
+    }
+
+    @Test
+    void kratNulovy() {
+        Zlomek a = new Zlomek(0, 4);
+        Zlomek b = new Zlomek(4, 5);
+        Zlomek c = a.krat(b);
+        assertEquals(0, c.getCitatel(), "Chybny citatel");
+        assertEquals(20, c.getJmenovatel(), "Chybny jmenovatel");
+    }
+
+    @Test
+    void deleno() {
+    }
+
+    @Test
+    void plus() {
+    }
+
+    @Test
+    void minus() {
+    }
+
+    @Test
+    void zkratit() {
+    }
+}
